40182914807d947210bf82d9d4d7666fc6f04ed4
khakimovSamdu <xakimov2806@gmail.com>
2025-07-28 09:40:35 -0800

    get data from api

---
+++ api/get_gosht_mahsulotlar.php
@@ -0,0 +1,20 @@
+<?php
+    include_once '../config.php';
+    $db = new Database();
+
+    $soyish_id = $_POST['id']; 
+    $query = "SELECT gsm.*, m.nomi AS mahsulot_nomi, m.narxi, m.tavsif
+            FROM gosht_soyish_mahsulot gsm
+            JOIN mahsulotlar m ON gsm.mahsulot_id = m.id
+            WHERE gsm.gosht_soyish_id = ".$soyish_id;
+    
+    $result = $db->query($query);
+
+    $data = [];
+    while ($row = $result->fetch_assoc()) {
+        $data[] = $row;
+    }
+
+    echo json_encode($data);
+
+?>
